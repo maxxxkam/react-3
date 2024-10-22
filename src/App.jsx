92707008a450f9c2e8ce38645f355a6653963b9f
maxxxkam <muslimovmaxkam@gmail.com>
2024-10-22 18:53:12 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
